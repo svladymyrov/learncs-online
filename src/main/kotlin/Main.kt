0e41bead9e9f395a56f517d329a53c651d7a2371
Serhii Vladymyrov <serhii.vladymyrov@gmail.com>
2023-10-09 09:38:14 +0300

    A few more solutions for the Fuction section

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -34,9 +34,70 @@ fun midThree(values: IntArray): IntArray {
   return values.slice(marginSize..marginSize+2).toIntArray()
 }
 
+fun calculateGrossProfit(revenue: IntArray, expenses: IntArray): Int {
+  var profit = 0
+  for (i in revenue.indices) {
+    profit += revenue[i] - expenses[i]
+  }
+  return profit
+}
+
+fun commonEnd(first: IntArray, last: IntArray): Boolean {
+  return first[0] == last[0] || first[first.lastIndex] == last[last.lastIndex]
+}
+
+fun nearTen(num: Int): Boolean {
+  return num % 10 in 0..2 || num % 10 in 8..9
+}
+
+fun multiply(a: Double, b: Double): Double {
+  return a * b
+}
+fun multiply(a: Double, b: Double, c: Double): Double {
+  return a * b * c
+}
+
+fun reversePrint(arr: CharArray): Int {
+  for (i in arr.size - 1 downTo 0) {
+    println(arr[i])
+  }
+  return arr.size
+}
+
+fun specialEleven(n: Int): Boolean {
+  return n % 11 in 0..1
+}
+
+fun sumDouble(a: Int, b: Int): Int {
+  val sum = a + b
+  return if (a != b) {
+    sum
+  } else {
+    sum * 2
+  }
+}
+
+// [5.0, 4.0, 4.5]
+fun catchupGrading(grades: DoubleArray): Int {
+  var count = 0
+  for (i in grades.indices) {
+    if (i == grades.lastIndex) continue
+    val grade = grades[i]
+    val nextGrade = grades[i + 1]
+    if (grade < nextGrade) {
+      grades[i] = (grade + nextGrade) / 2
+      count += 1
+    }
+  }
+  return count
+}
+
+
 /**
  * MAIN
  * */
 fun main(): Unit {
   println(midThree(intArrayOf(1,2,3,4,5)).joinToString(" "))
+  println(specialEleven(34))
+  println(catchupGrading(doubleArrayOf(5.0, 3.0, 4.5)))
 }
